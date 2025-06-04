dcc90a12d0853c1b137e7d9a91b6f118ebb38167
Ishanya Tripathi <166161759+ishanyatripathi@users.noreply.github.com>
2025-06-04 23:46:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,15 @@ document.getElementById("signup-form").addEventListener("submit", function (e) {
 
   fetch("https://script.google.com/macros/s/AKfycbyHnQ0FfFinqiU84vlNJApOqTdhLJW2IrTgNN6qDo0LEYP6vuOMFYKYnlcRRmMk1m4/exec", {
     method: "POST",
-    body: JSON.stringify({ email }),
+    body: JSON.stringify({ email: email }),
     headers: {
       "Content-Type": "application/json",
     },
   })
     .then(res => res.json())
-    .then(response => {
-      if (response.result === "success") {
-        alert("Thank you! You can now download C.O.R.E.");
-        window.location.href = "https://github.com/ishanyatripathi/CORE-Website/releases/download/v1.0/CORE_v1.zip";
-      } else {
-        alert("Something went wrong: " + response.message);
-      }
+    .then(() => {
+      alert("Thank you! You can now download C.O.R.E.");
+      window.location.href = "https://github.com/ishanyatripathi/CORE-Website/releases/download/v1.0/CORE_v1.zip";
     })
     .catch(err => {
       alert("Something went wrong.");
