04c0b4cf06226d0aeece4494f8bb73a03aa19464
Ishanya Tripathi <166161759+ishanyatripathi@users.noreply.github.com>
2025-06-05 00:55:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 document.getElementById("signup-form").addEventListener("submit", function (e) {
   e.preventDefault();
+
   const email = document.getElementById("email").value;
   if (!email) {
     alert("Please enter a valid email.");
     return;
   }
 
-  fetch(`https://script.google.com/macros/s/AKfycbyHnQ0FfFinqiU84vlNJApOqTdhLJW2IrTgNN6qDo0LEYP6vuOMFYKYnlcRRmMk1m4/exec?email=${encodeURIComponent(email)}`, {
+  const url = "https://script.google.com/macros/s/AKfycbyHnQ0FfFinqiU84vlNJApOqTdhLJW2IrTgNN6qDo0LEYP6vuOMFYKYnlcRRmMk1m4/exec";
+  const fetchUrl = `${url}?email=${encodeURIComponent(email)}`;
+
+  fetch(fetchUrl, {
     method: "GET",
   })
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => {
+      if (!res.ok) throw new Error("Network response was not ok: " + res.status);
+      return res.json();
+    })
+    .then((data) => {
       if (data.result === "success") {
         alert("Thank you! You can now download C.O.R.E.");
         window.location.href = "https://github.com/ishanyatripathi/CORE-Website/releases/download/v1.0/CORE_v1.zip";
@@ -18,7 +25,7 @@ document.getElementById("signup-form").addEventListener("submit", function (e) {
         alert("Server error: " + data.message);
       }
     })
-    .catch(err => {
+    .catch((err) => {
       alert("Something went wrong: " + err.message);
       console.error(err);
     });
